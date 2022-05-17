8f41a92b44363cc3c6391e72b92727ec56c36ee6
saken rai <saken.rai.34@gmail.com>
2022-05-18 00:20:39 +0545

    mui added

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ import { Container } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <Container>
-        <h1>Contact</h1>
-        <h3><a href = "mailto: saken.rai.34@gmail.com">Send Email</a></h3>
-        <h1>Projects</h1>
-        <div className="Projects">
-          <ProjectCard url={"https://www.vibeseducation.com/"} />
-          <ProjectCard url={"https://strong-sunflower-ff499e.netlify.app/"} />
-        </div>
-      </Container>
+      <h3>
+        <a href="mailto: saken.rai.34@gmail.com">Send Email</a>
+      </h3>
+      <h1>Projects</h1>
+      <div className="Projects">
+        <ProjectCard url={"https://www.vibeseducation.com/"} />
+        <ProjectCard url={"https://strong-sunflower-ff499e.netlify.app/"} />
+      </div>
     </div>
   );
 }
