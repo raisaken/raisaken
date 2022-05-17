47b642e038d5c0387b278bdec16d1b97170eeeb3
saken rai <saken.rai.34@gmail.com>
2022-05-18 00:30:37 +0545

    react-bootstrap removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import ProjectCard from "./Components/Card/ProjectCard";
-import { Container } from "react-bootstrap";
+
 
 function App() {
   return (
